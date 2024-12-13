69330ee67660e469256a001ebb51887e6dc3472a
Max Sanchez <maxsanchez9819@gmail.com>
2024-12-14 00:50:29 +0100

    "Minor .cmd changes for cm4"

--- hubbard2d.cmd
+++ hubbard2d.cmd
@@ -1,17 +1,20 @@
 #!/bin/bash
-#SBATCH -J hubb2d
+#SBATCH -J mpi_bench
 #SBATCH -o ./%x.%j.%N.out
 #SBATCH -D ./build/
 #SBATCH --mail-user=max.sanchez-zumpe@tum.de
 #SBATCH --mail-type=ALL
 #SBATCH --get-user-env
-#SBATCH --clusters=cm2
-#SBATCH --partition=cm2_std
-#SBATCH --nodes=16
+#SBATCH --clusters=cm4
+#SBATCH --partition=cm4_std
+#SBATCH --qos=cm4_std
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=112
 #SBATCH --export=NONE
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 
 module load slurm_setup
-export OMP_NUM_THREADS=28
+module load intel-mpi
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 mpiexec -n $SLURM_NTASKS ./hubbard2d.exe
