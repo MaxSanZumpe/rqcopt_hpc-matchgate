a93fc6dd69bd5f8eb0d325668893f6f53cb6e64c
Max Snchez Zumpe <ge47gej2@cm4login1.cos.lrz.de>
2024-12-30 22:07:59 +0100

    Running optimizations

--- examples/hubbard1d/hubbard1d_opt.c
+++ examples/hubbard1d/hubbard1d_opt.c
@@ -56,19 +56,19 @@ static int ufunc(const struct statevector* restrict psi, void* fdata, struct sta
 int main()
 {
 	const int nqubits = 8;
-	const int nlayers = 61;
+	const int nlayers = 57;
 	
 	const int full_target = 1;
 	const int ulayers = 29;
 	
 	if (full_target == 1) { assert(ulayers == 29); }
 
-	char splitting[] = "suzuki4";
+	char splitting[] = "auzinger6";
 
 	float g = 1.50;
     float t = 0.25;
 
-	const int niter = 10;
+	const int niter = 30;
 
 	int num_threads;
 	#if  defined(STATEVECTOR_PARALLELIZATION) || defined(GATE_PARALLELIZATION)
