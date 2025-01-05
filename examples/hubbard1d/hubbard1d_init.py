e4ac896a2eeebaa5243677fa5cada371e04356a9
Max Snchez Zumpe <ge47gej2@cm4login2.cos.lrz.de>
2025-01-05 01:08:27 +0100

    Running opts

--- examples/hubbard1d/hubbard1d_init.py
+++ examples/hubbard1d/hubbard1d_init.py
@@ -33,7 +33,7 @@ def construct_hubbard_interac_term(U):
 
     return U*np.kron(n, n)
 
-L = 4
+L = 6
 nqubits = 2*L
 J = 1
 
@@ -42,11 +42,11 @@ full_matrix = False
 if (full_matrix):
     assert(nqubits <= 12)
 
-g = 1.5
-t = 0.25
+g = 4.0
+t = 1.00
 
-s = 35
-us = 1
+s = 9
+us = 21
 
 
 if (full_matrix):
